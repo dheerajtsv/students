605732eefb2317bcc18ce85be2bc68f756333f91
Sai Venkata Dheeraj Talasila <78401163+dheerajtsv@users.noreply.github.com>
2026-06-04 21:57:43 +0000

    removed selectbox

--- 6.py
+++ 6.py
@@ -111,20 +111,19 @@ with tab3:
         gen = st.slider('Input General Management Score (out of 50)',min_value=0,max_value=50)
         dom = st.slider('Domain Specific Score (out of 50)',min_value=0,max_value=50)
         total = st.slider('Total Score (out of 100)',min_value=0,max_value=100)
-        selectBox = st.checkbox('Would the student like to consent to promotion of him for masters?')
         submitted = st.form_submit_button('Submit')
         if submitted:
             temp = 'No'
             if selectBox:
                 temp='Yes'
-            data = (name,university,program, honours, semester, domain, gen, dom, total, selectBox, temp)
+            data = (name,university,program, honours, semester, domain, gen, dom, total, temp)
             con = sql.connect('student.db')
             cur = con.cursor()
             try:
                 check = f'SELECT "NAME OF THE STUDENT" FROM private WHERE "NAME OF THE STUDENT"="{name}"'
                 if pd.read_sql(check,con).size == 0:
 
-                    cur.execute('''INSERT INTO private VALUES(?,?,?,?,?,?,?,?,?,?,?)''',data)
+                    cur.execute('''INSERT INTO private VALUES(?,?,?,?,?,?,?,?,?,?)''',data)
                     con.commit()
                     st.success("Data submitted successfully")
                 else:
